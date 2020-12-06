8e78a125cc8b69fb7ca9e2f6dbd18f6dc8c3dba8
Surfury <68794693+Surfury@users.noreply.github.com>
2020-12-06 10:40:29 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const colour=document.getElementById("color"),rainbow=document.getElementById("rainbow"),
+    reset=document.getElementById("reset"),resize=document.getElementById("resize"),
+    container = document.querySelector("div.grid");
+let root=document.documentElement,cell=[];
+
+reset.addEventListener('click',() => {
+    document.querySelectorAll(".filled").forEach(e => e.removeAttribute("style"));
+});
+
+function createGrid(squaresPerSide) {
+
+    container.style.gridTemplateColumns=(`repeat(${squaresPerSide}, 1fr`);
+    container.style.gridTemplateRows=(`repeat(${squaresPerSide}, 1fr`);
+
+    let numberOfCells=resize.value*resize.value;
+    for (let i=0;i<numberOfCells;i++) {
+        cell[i]=document.createElement('span');
+        container.appendChild(cell[i]);
+    }
+    document.querySelectorAll("span").forEach(E => E.addEventListener('mouseover',(M) => {
+        if (M.ctrlKey) {
+            if (rainbow.checked===false) {
+                M.target.style.backgroundColor=colour.value;
+            } else if (rainbow.checked) {
+                M.target.style.backgroundColor=`hsl(${Math.random()*360}, 100%, 50%)`
+            }
+            M.target.classList.add("filled");
+        }
+    }));
+}
+createGrid(resize.value);
+resize.addEventListener('change',() => createGrid(resize.value));
